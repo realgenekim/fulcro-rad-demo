072abc3e8875d2219662382038ecbb7dae57ef26
Gene Kim <genek@realgenekim.me>
2020-12-25 20:17:35 -0800

    restrict tests.edn kaocha src, to avoid namespace reload error: connection-pools.clj (from SQL)

--- src/datomic-tests/com/example/model/gene_test.clj
+++ src/datomic-tests/com/example/model/gene_test.clj
@@ -28,6 +28,7 @@
 
 
 
+
 ;(println
 ;  ; this doesn't work -- no server running
 ;  (let [db (d/db (:video datomic-connections))]
@@ -113,6 +114,45 @@
                  :session/tags-2
                  (#(map (fn [x] (-> x
                                     first)) %)))))))
+
+  (def leartalk-retval
+    {:session/conf-sched-id "5348024557502565-119"
+     :session/conf-id {:db/id 5348024557502565
+                       :conference/name "Vegas-Virtual 2020"}
+     :session/venue "Track 3"
+     :session/title "#Culture Stole My OKR's! (Nationwide Building Society)"
+     :session/tags-2 [{:session-tag-2/id #uuid "95ec4b65-a7e1-4a94-91d5-5a3196b0b388"
+                       :session-tag-2/video-tag {:video-tag/id #uuid "5d81f6f7-a5a8-4196-aef6-4ba3ae125777"
+                                                 :video-tag/name "Leadership"}}
+                      {:session-tag-2/id #uuid "be1f1687-4700-4143-9274-001d3cfd506e"
+                       :session-tag-2/video-tag {:video-tag/id #uuid "1993c94e-5fe6-4aea-8eec-51c046a98b47"
+                                                 :video-tag/name "Structure and Dynamics"}}]
+     :session/sched-id 119
+     :session/tags [{:db/id 2713594698338703
+                     :session-tag/id #uuid "a6d956fb-a6c7-4922-9e8b-2a05d000dc1a"
+                     :session-tag/tag-id-2 #uuid "5d81f6f7-a5a8-4196-aef6-4ba3ae125777"
+                     :session-tag/session-eid-2 {:db/id 2291382232285303
+                                                 :session/uuid #uuid "63827c18-5960-408f-8421-66d121a175b2"
+                                                 :session/title "#Culture Stole My OKR's! (Nationwide Building Society)"}
+                     :session-tag/tag-eid-2 {:db/id 34199209671332235
+                                             :video-tag/id #uuid "5d81f6f7-a5a8-4196-aef6-4ba3ae125777"
+                                             :video-tag/name "Leadership"}}
+                    {:db/id 42282819158741392
+                     :session-tag/id #uuid "8f916b65-9346-4119-9dc7-8239f83f8597"
+                     :session-tag/tag-id-2 #uuid "1993c94e-5fe6-4aea-8eec-51c046a98b47"
+                     :session-tag/session-eid-2 {:db/id 2291382232285303
+                                                 :session/uuid #uuid "63827c18-5960-408f-8421-66d121a175b2"
+                                                 :session/title "#Culture Stole My OKR's! (Nationwide Building Society)"}
+                     :session-tag/tag-eid-2 {:db/id 42282819158741389
+                                             :video-tag/id #uuid "1993c94e-5fe6-4aea-8eec-51c046a98b47"
+                                             :video-tag/name "Structure and Dynamics"}}
+                    {:db/id 62848084644663699}]
+     :session/uuid #uuid "63827c18-5960-408f-8421-66d121a175b2"
+     :session/start-time-utc #inst "2020-10-14T18:35:00.000-00:00"
+     :session/type {:db/id 15942918602752074
+                    :db/ident :session-type/track}
+     :db/id 2291382232285303
+     :session/speakers "Peter Lear; Kimberley Wilson"})
 
   (testing "get lear by uuid: get all the video-tags"
     (let [r (myparse [{[:session/uuid #uuid "63827c18-5960-408f-8421-66d121a175b2"]
